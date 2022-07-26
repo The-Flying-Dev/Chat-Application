26c806a0c6b49352e6b3214db050cd796467efef
The-Flying-Dev <leandranm@outlook.com>
2022-07-26 01:16:29 -0700

    Posts Component

--- app/javascript/components/Posts.js
+++ app/javascript/components/Posts.js
@@ -4,6 +4,55 @@ import { useActionCable } from 'use-action-cable';
 
 const Posts = props => {
 
-}
+  //initial state
+  const [posts, setPosts] = useState(props.posts)
+
+  //handler of new data for the ActionCable subscription
+  const channelHandlers = {
+    received: data => {
+      console.log(`[ActionCable] [Posts]`, data);
+      //setting Posts function
+      setPosts(data);
+    }
+  };
+
+  //app\channels\posts_channel.rb
+  //ties the handler and channel name into a new front-end subscription
+  useActionCable({ channel: "PostsChannel" }, channelHandlers);
+
+  return (
+    <React.Fragment>
+      {posts.map(post => {
+        return (
+          <div key={post.id} className='card mb-2'>
+            <div className='card-body'>
+              <h5 className='card-title text-muted'>
+                <small className='float-right'>
+                  Posted at {post.created_at}
+                </small>
+                {post.username}
+              </h5>
+              <div className='card-text lead mb-2'>
+                {post.body}
+              </div>
+              <a className='card0link' href={`/posts/${post.id}/repost`}>
+                Repost ({post.repost_count})
+              </a>
+              <a className='card0link' href={`/posts/${post.id}/like`}>
+                Likes ({post.likes_count})
+              </a>
+            </div>
+          </div>
+        );
+      })}
+    </React.Fragment>
+  );
+};
+
+//used for passing read-only attributes to make sure props are of the correct data type
+Posts.PropTypes = {
+  posts: PropTypes.array,
+  header_display: PropTypes.string
+};
 
 export default Posts;
