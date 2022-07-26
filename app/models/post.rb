a80f03997e6c8c322bd4fded74ac53bb8122e99f
The-Flying-Dev <leandranm@outlook.com>
2022-07-26 01:37:55 -0700

    Override created_at method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
   #validations
   validates :body, length: { minimu: 1, maximum: 280 }
+
+  #override created_at everywhere
+  def created_at
+    attributes['created_at'].strftime("%m/%d/%Y %H:%M")
+  end
+
 end
